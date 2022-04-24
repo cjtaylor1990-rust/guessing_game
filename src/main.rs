9e12fdc2efe5258fb7197a0c96e5773ed47dc7ff
cjtaylor1990 <9288608+cjtaylor1990@users.noreply.github.com>
2022-04-24 00:25:32 -0400

    Finishing Guessing Game.
    Completed guessing game. Added loop
    around body of logic and using a match
    statement to compare the guess with the
    secret number.
    
    This is part of Ch.2 of TRPL.
    
    This has been tested manually.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,62 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    /*
+     * Generate a random u32 integer between 0 and 100.
+     *
+     * Sets as immutable secret_number variable.
+     */
+    let secret_number = rand::thread_rng().gen_range(0, 101);
+
+    // Declares beginning of the game.
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        // Creating guess as mutable String variable.
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // Taking in standard input and appending it to the
+        // guess String, taking it in as a mutable reference.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        /*
+         * Shadowing guess by defining a new guess which is u32.
+         * Trim and parse the String guess into u32 integer.
+         * Match with two branches of parse (Ok and Err).
+         * Will start loop over if Err.
+         */
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Printing out user guess.
+        println!("You guessed: {}", guess);
+
+        /*
+         * Comparing guess to secret number (immutable reference).
+         *
+         * Matches against three branches of cmp (which returns an Odering enum).
+         *
+         * If the guess is too small or too big, it will say so and redo the loop.
+         *
+         * If the guess is equal to the secret number, then it will declare that you have won
+         * and then exit the loop.
+         */
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                // On equal branch, print that user has one and break from loop.
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
